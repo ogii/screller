820ab9561067ae4d8cf77c33dd9e47a8226d5932
ogii <ordinallx@gmail.com>
2019-09-23 16:50:25 +0900

    Added isvisible function to check if element is visble

--- scrllerjs.js
+++ scrllerjs.js
@@ -1,15 +1,23 @@
-(function () {
-    var scroll = window.innerHeight;
-    window.addEventListener('scroll', function (e) {
-        var pos = window.scrollY + window.innerHeight;
-        if (scroll < pos) {
-            // code when scrolling down is here
-            if ((document.querySelector('/* put css selector for element here */').getBoundingClientRect().top + window.scrollY) < pos) {
-                //code to execute when top of element enters the screen
-            }
-        } else {
-            // code when scrolling up is here
-        }
-        scrollPos = pos;
-    });
+(function() {
+  var scroll = window.innerHeight;
+  window.addEventListener("scroll", function(e) {
+    var pos = window.scrollY + window.innerHeight;
+    if (scroll < pos) {
+      // code when scrolling down is here
+      var ele = document.querySelector("/* put css selector for element here */");
+      if (isVisible(ele) && ele.getBoundingClientRect().top + window.scrollY < pos) {
+        //code to execute when top of element enters the screen
+        this.alert('HEY');
+      }
+    } else {
+      // code when scrolling up is here
+    }
+    scrollPos = pos;
+  });
+
+  // check if the element is visible
+  function isVisible(element) {
+    if (!element) return false;
+    return element.offsetWidth > 0 || element.offsetHeight > 0;
+  }
 })();
