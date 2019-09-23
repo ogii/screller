ea90a69991a00ee38e04548e2773091565b73142
ogii <ordinallx@gmail.com>
2019-09-23 16:41:53 +0900

    Added logic for firing code when scrolling up, and updated inline comments

--- scrllerjs.js
+++ scrllerjs.js
@@ -3,10 +3,12 @@
     window.addEventListener('scroll', function (e) {
         var pos = window.scrollY + window.innerHeight;
         if (scroll < pos) {
-            // scrolling down only
+            // code when scrolling down is here
             if ((document.querySelector('/* put css selector for element here */').getBoundingClientRect().top + window.scrollY) < pos) {
                 //code to execute when top of element enters the screen
             }
+        } else {
+            // code when scrolling up is here
         }
         scrollPos = pos;
     });
